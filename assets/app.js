1d24cc9b2807ce8a436cfaf72372ae7773c5e3af
John Mish <johnmish25@gmail.com>
2019-10-29 10:01:01 -0400

    updated query URL in js

--- assets/app.js
+++ assets/app.js
@@ -1,80 +1,41 @@
-$(document).ready(function() {
+var topicsArray = ["tom hanks", "denzel washington", "leonardo dicaprio", "christian bale"]
+var actorImg = "";
+// ßvar keyword = "keywordSearch"
+var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=va98Ii4e8WfRbdMR0ItjXLCV2tTIVJRQ&q=" + keyword + "&limit=25&offset=0&rating=G&lang=en";
 
-var topics = []; 
+// $("#keyword-input").on("click", function () {
 
-
-
-      $("#actorSearch").on("click", function(event) {
-
-       
-        event.preventDefault();
-
-        
-        var actor = $("#actorSearch").val();
-        console.log (actorSearch)
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + actor + "&city&api_key=va98Ii4e8WfRbdMR0ItjXLCV2tTIVJRQ";
-
-       
-      },
-
-        $.ajax({
-          url: queryURL,
-          method: "GET"
-        }).then(function(response) {
-
-        var results = response.data;
-        console.log (results);
-
-        for (var i = 0; i < results.length; i++) {
-
-            var rating = results[i].rating;
-            var defaultAnimatedSrc = results[i].images.fixed_height.url;
-            var staticSrc = results[i].images.fixed_height_still.url;
-            var showImage = $("<img>")
-            var p = $("<p>").text("rating" + " ");
-
-           
-
-
-        }
-       
-          
-     
-
-
-
-
-
-
-
-        $(".gif").on("click", function() {
-            // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
-            var state = $(this).attr("data-state");
-            // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-            // Then, set the image's data-state to animate
-            // Else set src to the data-still value
-            if (state === "still") {
-              $(this).attr("src", $(this).attr("data-animate"));
-              $(this).attr("data-state", "animate");
-            } else {
-              $(this).attr("src", $(this).attr("data-still"));
-              $(this).attr("data-state", "still");
-            }
-        })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function (response) {
+    console.log(response);
 });
+
+
+
+
+
+
+    // .done(function(response) {
+
+    //     console.log(response);
+    //     var results = response.data;
+    //     for (var i = 0; i < results.length; i++) {
+    //         var gifDiv = $("<div class='item'>");
+    //         var p = $("<p>").text("Rating: " + rating);
+    //         var actorImg = $("<img>");
+    //         actorImg.attr("src", results[i].images.fixed_height_still.url);
+    //         actorImg.attr("data-still", results[i].images.fixed_height_still.url);
+    //         actorImg.attr("data-animate", results[i].images.fixed_height.url);
+    //         actorImg.attr("data-state", "still");
+    //         actorImg.addClass("img-fluid gif border border-primary");
+    //         gifDiv.prepend(p);
+    //         gifDiv.prepend(actorImg);
+    //     };
+    // })
+//insert if else statement here for static or dynamic gifs 
+    //$(".gif").on("click", function() {
+        //var state = $(this).attr("data-state");
+
+    //})
